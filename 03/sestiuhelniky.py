61f8d41c19611f05c83c89f530856f888b313aec
Lenka Kapinová <l.kapinova@gmail.com>
2019-03-17 22:57:12 +0100

    Lesson

---
+++ 03/sestiuhelniky.py
@@ -0,0 +1,10 @@
+from turtle import forward, shape, left, right, clear, exitonclick, penup, pendown
+
+for j in range(6):
+    for i in range(6):
+        left(60)
+        forward(50)
+    right(60)
+    forward(50)
+
+exitonclick()
