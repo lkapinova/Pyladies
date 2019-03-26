db136f0730e73f3e9672d6c62fb3824311442c0f
Lenka Kapinová <l.kapinova@gmail.com>
2019-03-26 23:18:41 +0100

    Home project

--- 04/dp_2.py
+++ 04/dp_2.py
@@ -4,6 +4,7 @@ import random
 print("Vítej ve hře s kostkou. Vítězí hráč, který bude potřebovat nejvíc hodů na to, aby mu padla šestka.")
 
 def hod_kostkou_6 ():
+    "Opakuje hod kostkou, dokud nepadne šestka. Sleduje počet hodů."
     pocet_hodu = 1
     while True:
         hod = random.randint(1,6)
@@ -12,10 +13,15 @@ def hod_kostkou_6 ():
             pocet_hodu += 1
         else:
             print("Hráč potřeboval", pocet_hodu, "hodů.")
-            return (pocet_hodu)
+            return pocet_hodu
 
-pocet_hracu = 2
+pocet_hracu = 4
+max_pocet_hodu = 1
 for i in range(pocet_hracu):
     pocet_hodu = hod_kostkou_6()
-
+    if pocet_hodu > max_pocet_hodu:
+        max_pocet_hodu = pocet_hodu
+        hrac = i+1
+print("Vyhrál hráč č.%d. Musel házet %dx." %(hrac, max_pocet_hodu))
+    
     
