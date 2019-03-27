6f9bee7dcd9b78aa55fc1c04e030ed564f353e71
Lenka Kapinová <l.kapinova@gmail.com>
2019-03-27 22:30:31 +0100

    Home project

--- 04/dp_12.py
+++ 04/dp_12.py
@@ -1,4 +1,4 @@
-for radek in range(5):
+for line in range(5):
     for x in range(5):
         print("x", end=' ')
     print()
---
+++ 04/dp_13.py
@@ -0,0 +1,4 @@
+for line in range(5):
+    for x in range(line+1):
+        print('x', end=' ')
+    print()
