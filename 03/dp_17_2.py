713f227e85dce724b6f069fcd44933924a9b9ece
Lenka Kapinová <l.kapinova@gmail.com>
2019-03-27 21:48:03 +0100

    Home project

--- 03/dp_17_2.py
+++ 03/dp_17_2.py
@@ -1,21 +1,11 @@
-print("Tento program vybere z pěti, tebou zadaných čísel nejmenší.")
+print("Tento program vybere z pěti tebou zadaných čísel nejmenší.")
 
-a = int(input("Zadej první číslo: "))
-b = int(input("Zadej druhé číslo: "))
-c = int(input("Zadej třetí číslo: "))
-d = int(input("Zadej čtvrté číslo: "))
-e = int(input("Zadej páté číslo: "))
-
-
-min = a
-if b < min:
-    min = b
-if c < min:
-    min = c
-if d < min:
-    min = d
-if e < min:
-    min = e
+cislo = int(input("Zadej číslo: "))
+min = cislo
+for i in range(4):
+    dalsi_cislo = int(input("Zadej číslo: "))
+    if dalsi_cislo < min:
+        min = dalsi_cislo
 
 print("Nejmenší číslo je ", min)
 
---
+++ 04/dp_7.py
@@ -0,0 +1,16 @@
+def ano_nebo_ne(otazka):
+    "Vrátí True nebo False, podle odpovědi uživatele"
+    while True:
+        odpoved = input(otazka)
+        odpoved = odpoved.lower
+        if odpoved == 'ano' or 'a':
+            return True
+        elif odpoved == 'ne'or 'n':
+            return False
+        else:
+            print('Nerozumím! Odpověz "ano" nebo "ne".')
+
+if ano_nebo_ne('Chceš si zahrát hru? '):
+    print('OK! Ale napřed si ji musíš naprogramovat.')
+else:
+    print('Škoda.')
