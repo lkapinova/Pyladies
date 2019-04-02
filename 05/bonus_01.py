6c9fb1707c4815d060a80031a7064f16fd409b9c
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-02 19:18:09 +0200

    Lesson

---
+++ 05/bonus_01.py
@@ -0,0 +1,17 @@
+# Z řetězce "Čokoláda" získej pomoci pristupu k
+# jednotlivym znakum nebo podretezcum nasledujici retezce:
+
+# Č
+# á
+# kolá
+# láda
+# a
+# Čoko
+
+string = 'Čokoláda'
+print(string[0])
+print(string[5])
+print(string[2:6])
+print(string[4:8])
+print(string[-1])
+print(string[:4])
