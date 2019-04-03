ace786efe7f4e3dc5602abd4097739948ea3e8fe
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-03 23:04:14 +0200

    Home project

---
+++ 05/dp_02.py
@@ -0,0 +1,25 @@
+# Napiš funkci vyhodnot, která dostane řetězec s herním polem 1-D piškvorek a vrátí jednoznakový řetězec podle stavu hry:
+
+# "x" – Vyhrál hráč s křížky (pole obsahuje "xxx")
+# "o" – Vyhrál hráč s kolečky (pole obsahuje "ooo")
+# "!" – Remíza (pole neobsahuje "-", a nikdo nevyhrál)
+# "-" – Ani jedna ze situací výše (t.j. hra ještě neskončila)
+
+def vyhodnot(piskvorky):
+    "Funkce vyhodnotí 1D piškvorky. Pro vítězství jsou nutné tři znaky v řadě."
+
+    if "xxx" in piskvorky:
+        vysledek = 'x'
+    elif "ooo" in piskvorky:
+        vysledek = 'o'
+    elif "-" not in piskvorky and "xxx" not in piskvorky and "ooo" not in piskvorky:
+        vysledek = "!"
+    else:
+        vysledek = '-'
+    return vysledek
+
+print(vyhodnot("xxoxoxoxoxxoxxoox"))
+print(vyhodnot("xxxoxoxox-xxoxxoo"))
+print(vyhodnot("xxoooxoxox-xxoxxoo-o--"))
+print(vyhodnot("----x--o--xx--oo-xo-oxo--"))
+print(vyhodnot("xxx-ooo-xxxox--xoox-x"))
---
+++ 05/dp_03.py
@@ -0,0 +1,21 @@
+# Napiš funkci tah, která dostane řetězec s herním polem, číslo políčka (0-19), a symbol (x nebo o) a vrátí herní pole (t.j. řetězec) s daným symbolem umístěným na danou pozici.
+
+# Hlavička funkce by tedy měla vypadat nějak takhle:
+
+# def tah(pole, cislo_policka, symbol):
+#     "Vrátí herní pole s daným symbolem umístěným na danou pozici"
+#     ...
+# Můžeš využít nějakou funkci, kterou jsme napsaly už na sraze?
+
+def tah(pole, cislo_policka, symbol):
+    "Vrátí herní pole s daným symbolem umístěným na danou pozici"
+    pole = '-'*pole
+    herni_pole = pole[:cislo_policka] + symbol + pole[(cislo_policka+1):]
+    return herni_pole
+
+herni_pole = tah(20, 7, 'x')
+print(herni_pole)
+herni_pole = tah(20, 0, 'o')
+print(herni_pole)
+herni_pole = tah(20, 19, 'x')
+print(herni_pole)
