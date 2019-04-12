c495c60c5739aa1362b7e1f743c64e03a3cca128
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-12 23:10:23 +0200

    Home project

--- 06/dp_06.py
+++ 06/dp_06.py
@@ -1,24 +1,35 @@
 # Napiš program, který se zeptá na 3 čísla a zjistí, jestli je jejich součet větší než 10. 
 # Dokážeš to napsat tak, aby funkce input byla v kódu zapsaná jen jednou? ;)
 
-cisla = input("Zadej tri cisla. Oddel je od sebe carkou (napr.: 3,6,7): ")
-a,b,c = cisla.split(',')
-a = int(a)
-b = int(b)
-c = int(c)
-
-if a+b+c > 10:
-    print("Součet čísel {}, {}, {} je větší než 10.".format(a,b,c))
-else:
-    print("Součet čísel {}, {}, {} je menší než 10.".format(a,b,c))
-
+# cisla = input("Zadej par cisel. Oddel je od sebe carkou (napr.: 3,6,7): ")
+# a = cisla.split(',')
 
 # soucet = 0
-# for i in range(3):
-#     cislo = int(input("Zadej cislo: "))
-#     soucet = soucet + cislo
+# for i in a:
+#     soucet += int(i)
 
-# if soucet > 10:
-#     print("Součet čísel je větší než 10.")
+# print(soucet)    
+
+#  --------------------------------------------------------------------------------
+
+#  cisla = input("Zadej tri cisla. Oddel je od sebe carkou (napr.: 3,6,7): ")
+# a,b,c = cisla.split(',')
+# a = int(a)
+# b = int(b)
+# c = int(c)
+
+# if a+b+c > 10:
+#     print("Součet čísel {}, {}, {} je větší než 10.".format(a,b,c))
 # else:
-#     print("Součet čísel je menší než 10.")
+#     print("Součet čísel {}, {}, {} je menší než 10.".format(a,b,c))
+
+#----------------------------------------------------------------------------------
+soucet = 0
+for i in range(3):
+    cislo = int(input("Zadej cislo: "))
+    soucet += cislo
+
+if soucet > 10:
+    print("Součet čísel je větší než 10.")
+else:
+    print("Součet čísel je menší nebo roven 10.")
