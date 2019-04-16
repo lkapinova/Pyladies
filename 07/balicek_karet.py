f2e9243865433c4120040191af09afe4680a7842
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-16 22:14:41 +0200

    Lesson

--- 07/balicek_karet.py
+++ 07/balicek_karet.py
@@ -3,7 +3,7 @@ barvy = ['♠', '♥', '♦', '♣']
 hodnoty = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 for barva in barvy:
     for hodnota in hodnoty:
-        balicek.append(hodnota + barva)
+        balicek.append(hodnota + " " + barva)
 
 print(balicek)
 # print(len(balicek))
