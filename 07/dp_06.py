b9177ee2cccd76aabf25b50ae892cf3bb81e263f
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-18 19:55:10 +0200

    Home project

--- 07/dp_06.py
+++ 07/dp_06.py
@@ -1,12 +1,11 @@
 # Napiš funkci, která převede římské číslice na číslo (int).
 
-rimska_soust_dvojice = ['CM', 'CD', 'XC', 'XL', 'IX', 'IV']
-arabska_soust_dvojice = [900, 400, 90, 40, 9, 4]
-rimska_soust = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
-arabska_soust = [1000, 500, 100, 50, 10, 5, 1]
-
 
 def prevod_rimskych_cisel(rimske_cislo):
+    rimska_soust_dvojice = ['CM', 'CD', 'XC', 'XL', 'IX', 'IV']
+    arabska_soust_dvojice = [900, 400, 90, 40, 9, 4]
+    rimska_soust = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+    arabska_soust = [1000, 500, 100, 50, 10, 5, 1]
     cislo1 = 0
     cislo2 = 0
 
@@ -21,7 +20,8 @@ def prevod_rimskych_cisel(rimske_cislo):
 
     return (cislo1+cislo2)
 
+
 print(prevod_rimskych_cisel("MCMLXXXII"))       # 1982
-print(prevod_rimskych_cisel("MMXIX"))           # 2019 
+print(prevod_rimskych_cisel("MMXIX"))           # 2019
 print(prevod_rimskych_cisel("MDCCLXIII"))       # 1763
 print(prevod_rimskych_cisel("MCMXL"))           # 1940
---
+++ 07/dp_06_1.py
@@ -0,0 +1,5 @@
+# Napiš funkci, která převede římské číslice na číslo (int).
+
+rimska_soustava = ['CM', 'CD', 'XC', 'XL', 'IX', 'IV', 'M', 'D', 'C', 'L', 'X', 'V', 'I']
+arabska_soustava = [900, 400, 90, 40, 9, 4, 1000, 500, 100, 50, 10, 5, 1]
+
