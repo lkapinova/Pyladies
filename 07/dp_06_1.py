a77bf11fa9c15193d00a64acee1cad10a6ee84a7
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-18 20:23:41 +0200

    Home project

--- 07/dp_06_1.py
+++ 07/dp_06_1.py
@@ -1,5 +1,27 @@
 # Napiš funkci, která převede římské číslice na číslo (int).
 
-rimska_soustava = ['CM', 'CD', 'XC', 'XL', 'IX', 'IV', 'M', 'D', 'C', 'L', 'X', 'V', 'I']
-arabska_soustava = [900, 400, 90, 40, 9, 4, 1000, 500, 100, 50, 10, 5, 1]
 
+def prevod_rimskych_cisel(rimske_cislo):
+    rimska_soustava = ['CM', 'CD', 'XC', 'XL', 'IX',
+                       'IV', 'M', 'D', 'C', 'L', 'X', 'V', 'I']
+    arabska_soustava = [900, 400, 90, 40, 9, 4, 1000, 500, 100, 50, 10, 5, 1]
+    arabske_cislo = 0
+
+    while len(rimske_cislo) != 0:
+        for i, j in zip(rimska_soustava, arabska_soustava):
+            if rimske_cislo.startswith(i):
+                arabske_cislo += j
+                if len(i) == 2:
+                    rimske_cislo = rimske_cislo[2:]
+                    break
+                else:
+                    rimske_cislo = rimske_cislo[1:]
+                    break
+
+    return arabske_cislo
+
+
+print(prevod_rimskych_cisel("MCMLXXXII"))       # 1982
+print(prevod_rimskych_cisel("MMXIX"))           # 2019
+print(prevod_rimskych_cisel("MDCCLXIII"))       # 1763
+print(prevod_rimskych_cisel("MCMXL"))           # 1940
