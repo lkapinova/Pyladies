79bfbbb62be5e2c28b2ca17d4548b1a657b10713
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-19 20:41:47 +0200

    Bonus exercise

--- 07/bonus_04.py
+++ 07/bonus_04.py
@@ -1,2 +1,20 @@
-# Napis funkci, ktera dostane seznam a vrati True 
+# Napis funkci, ktera dostane seznam a vrati True
 # pokud obsahuje nejake duplicitni hodnoty, jinak vrati false
+
+
+def find_duplicates(list):
+    "Tato funkce hleda duplicitni hodnoty."
+    pocet_prvku = len(list)
+    list = set(list)
+    pocet_prvku_set = len(list)
+
+    if pocet_prvku == pocet_prvku_set:
+        return False
+    else:
+        return True
+
+
+a = [1, 3, 5, 7, 2, 5, 8, 13, 21, 34, 55, 89]
+b = [15, 16, 17, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(find_duplicates(a))
+print(find_duplicates(b))
--- 07/bonus_05.py
+++ 07/bonus_05.py
@@ -1,2 +1,20 @@
-# Napis funkci, ktera dostane seznam celych cisel a vrati novy seznam, 
+# Napis funkci, ktera dostane seznam celych cisel a vrati novy seznam,
 # ktery obsahuje jen ta cisla, ktera jsou kladna
+
+import random
+
+
+def seznam_kladnych_cisel(seznam):
+
+    kladna_cisla = []
+
+    for cislo in seznam:
+        if cislo >= 0:
+            kladna_cisla.append(cislo)
+
+    return kladna_cisla
+
+
+muj_seznam = random.sample(range(-100, 100), 30)
+print(muj_seznam)
+print(seznam_kladnych_cisel(muj_seznam))
