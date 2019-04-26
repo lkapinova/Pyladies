ea1f01bc4dc9c613fca9a3edae2529c15a8f4bc2
Lenka Kapinová <l.kapinova@gmail.com>
2019-04-26 13:52:59 +0200

    Home project

--- 08/dp_07.py
+++ 08/dp_07.py
@@ -37,18 +37,23 @@ def pohyb(seznam_souradnic, svetova_strana):
         x += 1
     elif svetova_strana == 'z':
         x -= 1
+
     nove_souradnice = (x, y)
     seznam_souradnic.append(nove_souradnice)
 
 
 seznam = [(0, 0), (1, 0), (2, 0)]
 rozmer_mapy = 10
+svetove_strany = ['s', 'j', 'z', 'v']
 
 while True:
     svetova_strana = input(
-        "Zadej světovou stranu (s,j,v,z). Pokud už nechceš pokračévat, napiš exit.: ")
+        "Zadej světovou stranu (s,j,v,z). Pokud už nechceš pokračovat, napiš exit.: ")
     if svetova_strana == 'exit':
         break
+    elif svetova_strana not in svetove_strany:
+        print("Nezadal jsi svetovou stranu správně. Zkus to znovu.")
+        continue
     else:
         pohyb(seznam, svetova_strana)
         nakresli_mapu(rozmer_mapy, seznam)
--- 08/dp_08.py
+++ 08/dp_08.py
@@ -1,12 +1,13 @@
-# Doplň funkci pohyb tak, aby při pohybu umazala první bod ze seznamu souřadnic. 
+# Doplň funkci pohyb tak, aby při pohybu umazala první bod ze seznamu souřadnic.
 # Výsledný seznam tak bude mít stejnou délku, jako před voláním.
 
 # Uprav testy.
 
+
 def pohyb(seznam_souradnic, svetova_strana):
     """Funkce ze seznamu souřadnic a světové strany (zadané jako: "s", "j", "v" nebo "z")
      a přidá k seznamu souřadnice bodu posunutý v zadaném směru."""
-    
+
     x = seznam_souradnic[-1][0]
     y = seznam_souradnic[-1][1]
 
@@ -20,15 +21,16 @@ def pohyb(seznam_souradnic, svetova_strana):
         x -= 1
     nove_souradnice = (x, y)
 
+    del seznam_souradnic[0]
     seznam_souradnic.append(nove_souradnice)
 
 
-souradnice = [(0, 0)]
-pohyb(souradnice, 'v')
-print(souradnice)         # → [(0, 0), (1, 0)]
-pohyb(souradnice, 'v')
-print(souradnice)         # → [(0, 0), (1, 0), (2, 0)]
+souradnice = [(0, 0), (1, 0), (2, 0)]
 pohyb(souradnice, 'j')
-print(souradnice)         # → [(0, 0), (1, 0), (2, 0), (2, 1)]
+print(souradnice)         # → [(1, 0), (2, 0), (2, 1)]
 pohyb(souradnice, 's')
-print(souradnice)         # → [(0, 0), (1, 0), (2, 0), (2, 1), (2, 0)]
+print(souradnice)         # → [(2, 0), (2, 1), (2, 0)]
+pohyb(souradnice, 'v')
+print(souradnice)         # → [(2, 1), (2, 0), (3, 0)]
+pohyb(souradnice, 'v')
+print(souradnice)         # → [(2, 0), (3, 0), (4, 0)]
