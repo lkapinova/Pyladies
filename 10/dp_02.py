e1557ab655d120a4aec114b80bd68fc6544a75ba
Lenka Kapinová <l.kapinova@gmail.com>
2019-05-09 21:53:26 +0200

    First issue

--- 10/dp_02.py
+++ 10/dp_02.py
@@ -6,13 +6,21 @@ class RodneCislo:
     def __str__(self):
         return self.hodnota
 
-    def zkotroluj_format(self):
+    def zkontroluj_format(self):
+        for _ in self.hodnota:
+            if _.isalpha():
+                return False
+
         if '/' in self.hodnota:
             divided_rc = self.hodnota.split('/', 1)
             if len(divided_rc[0]) == 6 and len(divided_rc[1]) == 4:
                 return True
+            else:
+                return False
+        else:
+            return False
 
-    def zkotroluj_delitelnost_11(self):
+    def zkontroluj_delitelnost_11(self):
         self.hodnota = self.hodnota[:6]+self.hodnota[7:]
         if int(self.hodnota) % 11 == 0:
             return True
@@ -20,7 +28,6 @@ class RodneCislo:
             return False
 
     def vypis_datum_narozeni(self):
-
         rok = int(self.hodnota[:2])
         if 54 <= rok <= 99:
             rok = 1900 + rok
@@ -45,11 +52,13 @@ class RodneCislo:
 
 rc_1 = RodneCislo('995211/4799')
 rc_2 = RodneCislo('000516/9185')
-print(rc_1.zkotroluj_format())
-print(rc_1.zkotroluj_delitelnost_11())
+rc_3 = RodneCislo('abcdef/abcd')
+print(rc_1.zkontroluj_format())
+print(rc_1.zkontroluj_delitelnost_11())
 print(rc_1.vypis_datum_narozeni())
 print(rc_1.zjisti_pohlavi())
-print(rc_2.zkotroluj_format())
-print(rc_2.zkotroluj_delitelnost_11())
+print(rc_2.zkontroluj_format())
+print(rc_2.zkontroluj_delitelnost_11())
 print(rc_2.vypis_datum_narozeni())
 print(rc_2.zjisti_pohlavi())
+print(rc_3.zkontroluj_format())
--- 10/dp_03.py
+++ 10/dp_03.py
@@ -3,7 +3,10 @@ from dp_02 import RodneCislo
 vstup = input("Zadej rodne cislo: ")
 
 rc = RodneCislo(vstup)
-print(rc.zkotroluj_format())
-print(rc.zkotroluj_delitelnost_11())
-print(rc.vypis_datum_narozeni())
-print(rc.zjisti_pohlavi())
+
+if rc.zkontroluj_format():
+    print(rc.zkontroluj_delitelnost_11())
+    print(rc.vypis_datum_narozeni())
+    print(rc.zjisti_pohlavi())
+else:
+    print("Spatny format rodneho cisla")
