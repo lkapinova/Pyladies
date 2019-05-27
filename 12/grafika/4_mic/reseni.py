dea0df1cf86e7b3f088c4eff008ed8e4d3cc950f
Lenka Kapinová <l.kapinova@gmail.com>
2019-05-27 21:26:34 +0200

    Home project

--- 12/grafika/4_mic/reseni.py
+++ 12/grafika/4_mic/reseni.py
@@ -1,26 +1,21 @@
 import pyglet
 
 window = pyglet.window.Window(width=1000, height=700)
-micek = pyglet.image.load("C:\\Users\\lenka\\main\\git\\Pyladies\\12\\grafika\\obrazky\\micek.png")
+micek = pyglet.image.load("./obrazky/micek.png")
 
 # (1) TODO: Vytvorit tridu Micek. Obsahovat bude odkaz na objekt na platne a rychlost tohoto objektu na obou osach rychlost_x a rychlost_y
 # (2) TODO: Rychlost na obou osach prijimat jako parametr
-
-
 class Micek():
-    
-    def __init__(self, rychlost_x, rychlost_y):
-        self.objekt = pyglet.sprite.Sprite(micek)
+    objekt = pyglet.sprite.Sprite(micek)
+
+    def __init__(self, rychlost_x=0, rychlost_y=4):
         self.rychlost_x = rychlost_x
         self.rychlost_y = rychlost_y
 
-
 objekty = []
 # (3) TODO: Do seznamu objektu vlozit instanci objektu Micek
-m1 = Micek(3, 3)
-m2 = Micek(1, 2)
-objekty.append(m1)
-objekty.append(m2)
+objekty.append(Micek(4,1))
+objekty.append(Micek(1,4))
 
 def vykresli():
     window.clear()
@@ -29,22 +24,17 @@ def vykresli():
     for element in objekty:
         element.objekt.draw()
 
-
-def pohyb(dt):
+def pohyb(t):
     # (5) TODO: Pri kazdem tiku posunu kazdy z elementu na ose X i Y
+    # (6) TODO: Pokud se element dostane na okraj okna, odrazi se (take na obou osach)
     for element in objekty:
         element.objekt.x += element.rychlost_x
         element.objekt.y += element.rychlost_y
-        
-        element.rychlost_y -= 1
 
-
-    # (6) TODO: Pokud se element dostane na okraj okna, odrazi se (take na obou osach)
-        if element.objekt.x < 0 or element.objekt.x > window.width - element.objekt.width:
-            element.rychlost_x = - element.rychlost_x 
-        if element.objekt.y < 0 or element.objekt.y > window.height - element.objekt.height:
+        if element.objekt.y > window.height - element.objekt.height or element.objekt.y < 0 :
             element.rychlost_y = - element.rychlost_y
-
+        if element.objekt.x > window.width - element.objekt.width or element.objekt.x < 0 :
+            element.rychlost_x = - element.rychlost_x
 
 pyglet.clock.schedule_interval(pohyb, 1/60)
 window.push_handlers(on_draw=vykresli)
