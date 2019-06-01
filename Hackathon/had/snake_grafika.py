d110b090f2742b47b1ab0b6e956a9e6a2f2c3f4e
Lenka Kapinová <l.kapinova@gmail.com>
2019-06-01 15:57:32 +0200

    Ji ovoce

--- Hackathon/had/snake_grafika.py
+++ Hackathon/had/snake_grafika.py
@@ -1,4 +1,5 @@
 import pyglet
+import random
 from pyglet.window import key
 
 
@@ -6,11 +7,12 @@ square_side = 64
 width = 15
 height = 10
 picture = pyglet.image.load('green.png')
+picture_apple = pyglet.image.load('red.png')
 svetova_strana = 'v'
 game_is_running = True
 
 
-class Snake():
+class Pixel():
 
     def __init__(self, picture, x, y):
         self.picture = picture
@@ -24,10 +26,23 @@ class Snake():
         return self.coord_x == other.coord_x and self.coord_y == other.coord_y
 
 
-def place_snake():
+class Apple(Pixel):
+
+    def __init__(self, picture, x, y):
+        self.picture = picture_apple
+        self.coord_x = x
+        self.coord_y = y
+
+
+def place_pixels():
     window.clear()
 
     for item in snake_coords:
+        piece = pyglet.sprite.Sprite(
+            item.picture, item.coord_x*square_side, item.coord_y*square_side)
+        piece.draw()
+
+    for item in apples_coords:
         piece = pyglet.sprite.Sprite(
             item.picture, item.coord_x*square_side, item.coord_y*square_side)
         piece.draw()
@@ -36,7 +51,7 @@ def place_snake():
 def pohyb(seznam_poli, svetova_strana):
     """Funkce ze seznamu souřadnic a světové strany (zadané jako: "s", "j", "v" nebo "z")
      a přidá k seznamu souřadnice bodu posunutý v zadaném směru."""
-    
+
     global game_is_running
 
     x = seznam_poli[-1].coord_x
@@ -52,19 +67,23 @@ def pohyb(seznam_poli, svetova_strana):
     elif svetova_strana == 'z':
         x -= 1
 
-    nove_pole = Snake(picture, x, y)
+    new_snake_pixel = Pixel(picture, x, y)
 
-    if x <= -1 or x > width-1 or y <= -1 or y > height-1:  
+    if x <= -1 or x > width-1 or y <= -1 or y > height-1:
         game_is_running = False
         return
 
-    del seznam_poli[0]
+    # pro porovnani se seznamem ovoce
+    new_pixel = Apple(picture, x, y)
+    if new_pixel in apples_coords:
+        apples_coords.remove(new_pixel)
+    else:
+        del seznam_poli[0]
 
-    if nove_pole in seznam_poli:
+    if new_snake_pixel in seznam_poli:
         game_is_running = False
-        
 
-    seznam_poli.append(nove_pole)
+    seznam_poli.append(new_snake_pixel)
 
 
 def stisk_klavesy(symbol, modifikatory):
@@ -79,19 +98,31 @@ def stisk_klavesy(symbol, modifikatory):
         svetova_strana = 'v'
 
 
+def add_apple(snake_coords, apples_coords):
+    while len(apples_coords) < 3:
+        x = random.randrange(width-1)
+        y = random.randrange(height-1)
+        new_apple = Apple(picture_apple, x, y)
+        if new_apple not in snake_coords and new_apple not in apples_coords:
+            apples_coords.append(new_apple)
+            
+
+
 def tik(time):
     if game_is_running:
+        add_apple(snake_coords, apples_coords)
         pohyb(snake_coords, svetova_strana)
+        print(snake_coords)
 
+snake_coords = [Pixel(picture, 0, 5), Pixel(picture, 1, 5), Pixel(
+    picture, 2, 5), Pixel(picture, 3, 5), Pixel(picture, 4, 5), Pixel(picture, 5, 5)]
+apples_coords = []
 
-snake_coords = [Snake(picture, 0, 5), Snake(picture, 1, 5), Snake(
-    picture, 2, 5), Snake(picture, 3, 5), Snake(picture, 4, 5), Snake(picture, 5, 5)]
-
-
+add_apple(snake_coords, apples_coords)
 pyglet.clock.schedule_interval(tik, 1)
 window = pyglet.window.Window(
     width=width*square_side, height=height*square_side)
-window.push_handlers(on_draw=place_snake,
+window.push_handlers(on_draw=place_pixels,
                      on_key_press=stisk_klavesy)
 
 pyglet.app.run()
